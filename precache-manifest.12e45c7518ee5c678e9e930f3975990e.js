cae4d5db9642b72853c6b1d7e4eba2e1f13a7839
narender bisht <narender007bisht@gmail.com>
2020-11-11 02:30:36 -0800

    Updates

---
+++ precache-manifest.12e45c7518ee5c678e9e930f3975990e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c0dbc86f7fb5dda7da629ef4c0e8398",
+    "url": "/food-app/index.html"
+  },
+  {
+    "revision": "a369d234df86b0a44d37",
+    "url": "/food-app/static/css/main.62532236.chunk.css"
+  },
+  {
+    "revision": "6b39e92519e1f366c2c3",
+    "url": "/food-app/static/js/2.2a710693.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/food-app/static/js/2.2a710693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a369d234df86b0a44d37",
+    "url": "/food-app/static/js/main.81ae7478.chunk.js"
+  },
+  {
+    "revision": "cdb3d0c8cb66b0700df5",
+    "url": "/food-app/static/js/runtime-main.d0367244.js"
+  }
+]);
