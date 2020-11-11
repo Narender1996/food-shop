88d774208ced7a7d8c7a688e8a21c3435c71497d
narender bisht <narender007bisht@gmail.com>
2020-11-11 02:49:08 -0800

    router change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles/App.css";
 import Home from './component/pages/home';
 import Addproduct from './component/products/addproduct'
 import Editproduct from './component/products/editproduct'
-import {BrowserRouter,Switch,Route} from 'react-router-dom';
+import {HashRouter,Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
